9f85c3b79d5a36ab63a41b2055344ed671d899e9
zerobug110 <mahabojang110@gmail.com>
2023-03-15 06:38:55 +0000

    return json instead of actual text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-res.status(200).send('hello from the server side')
+res.status(200).json({ message: 'hello from the server side', app: 'natour'})
 })
 const port = 3000
 
