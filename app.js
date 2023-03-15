e34def72e01ba94a57828f0b699b07331670ee9d
zerobug110 <mahabojang110@gmail.com>
2023-03-15 06:51:48 +0000

    added post request

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const app = express();
 app.get('/', (req, res)=>{
 res.status(200).json({ message: 'hello from the server side', app: 'natour'})
 })
-const port = 3000
 
+app.post('/post', (req, res) => {
+    res.status(200).send('you can send post to this endpoin')
+})
+
+
+const port = 3000
 app.listen(port, ()=>{
     console.log(`app runing on port ${port}...`)
 })
