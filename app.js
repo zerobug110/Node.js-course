7b8456e92f6f922421949b69c7af61cdef80f2cc
zerobug110 <mahabojang110@gmail.com>
2023-03-16 03:40:17 +0000

    created api/v1/tour endpoin

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-res.status(200).json({ message: 'hello from the server side', app: 'natour'})
-})
+// app.get('/', (req, res)=>{
+// res.status(200).json({ message: 'hello from the server side', app: 'natour'})
+// })
 
-app.post('/post', (req, res) => {
-    res.status(200).send('you can send post to this endpoin')
-})
+// app.post('/post', (req, res) => {
+//     res.status(200).send('you can send post to this endpoin')
+// })
 
+app.get('/api/v1/tours', (req, res) => {
+    res
+    .status(200).send('message from the API');
+})
 
 const port = 3000
 app.listen(port, ()=>{
