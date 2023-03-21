e3e2ea3950982af87f0088cddf2e9b24d563628f
zerobug110 <mahabojang110@gmail.com>
2023-03-21 03:03:29 +0000

    added data to the backend

--- app.js
+++ app.js
@@ -12,8 +12,19 @@ const tours = JSON.parse(
 
 app.post('/api/v1/tours', (req, res) => {
     // console.log(req.body)
-    const newId = tours[tours.length -1].id + 1
-    res.send('done')
+    const newId = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours), err => {
+        res.status(201).json({
+            staus: "success",
+            data: {
+                tours: newTour
+            }
+        })
+    })
+    // res.send('done')
 })
 
 app.get('/api/v1/tours', (req, res) => {
