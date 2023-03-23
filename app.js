8a08e8bf4b1fbab6a75786ae3671097c20c48bf2
zerobug110 <mahabojang110@gmail.com>
2023-03-23 05:59:50 +0000

    fixed the bug in morgan  middleware

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const morgan = require('morgan')
 
 const app = express();
 
+
+//MIDDLEWARES
+
+app.use(morgan('dev'));
+
 app.use(express.json())
 
 app.use((req, res, next) => {
     console.log('hello from the middlleware');
     next()
-})
-
-app.userr ((req, res, next) => {
-    req.requestTime = new Date().tosISOString;
-    next();
 })
 
 
