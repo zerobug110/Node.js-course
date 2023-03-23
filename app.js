8cd977629986dd4a5cdec249805dec1e3adfafd6
zerobug110 <mahabojang110@gmail.com>
2023-03-23 01:23:32 +0000

    installed morgan package

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 })
 
 app.userr ((req, res, next) => {
-    req.requestTime = new Date().ttosISOString;
+    req.requestTime = new Date().tosISOString;
     next();
 })
 
