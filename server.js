8ffa96597e6aaa4fc8c6ce2b6ad636f1ff7705de
zerobug110 <mahabojang110@gmail.com>
2023-03-27 05:18:36 +0000

    setup supportdesk file

--- server.js
+++ server.js
@@ -1,9 +1,1 @@
-const dotenv = require('dotenv');
-const app = require('./app');
-
-dotenv.config({ path: './config.env' });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+console.log("hello from the server")
