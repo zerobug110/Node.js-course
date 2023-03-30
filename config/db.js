7d26ca18cf6711d0dbd02b2238d77195b75a2d12
zerobug110 <mahabojang110@gmail.com>
2023-03-30 04:02:15 +0000

    fixed the mongodb connection bug

--- config/db.js
+++ config/db.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGGO_URI) 
-        console.log(`MonggoDB Connect: ${error}`);
+        const conn = await mongoose.connect(process.env.MONGO_URI) 
+        console.log(`MonggoDB Connected: ${conn.connection.host}.cyaan.underline`);
     } catch (error) {
-        
+        console.log(`Error: ${error.message}.` .red.underline.bold);
+        process.exit(1)
     }
 }
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
+const colors= require("colors");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/ueser-route");
 const errorHanddler = require("./middleware/error-middleware")
-
+const connectDB = require("./config/db")
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+//connect to database
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
